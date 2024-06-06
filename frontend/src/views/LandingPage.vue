7b4e94fd56b424eb392d75774e95e07649f70899
Nandya Nurmala <nandya1707@gmail.com>
2024-06-06 21:55:05 +0700

    feat(layout): update Landing Page (Headline, Terpopuler, Category)

--- frontend/src/views/LandingPage.vue
+++ frontend/src/views/LandingPage.vue
@@ -1,60 +1,282 @@
-<template>
+  <template>
     <div class="container margin-top">
       <div class="row">
         <div class="col-9">
           <h5 class="title">Headline News</h5>
           <div class="image-news">
             <img src="https://awsimages.detik.net.id/community/media/visual/2024/03/29/vina-sebelum-7-hari_169.jpeg?w=1200" alt="">
-            <b><div class="image-caption">Fakta Terkini Kasus Vina Cirebon, Polemik Pegi hingga Langkah Hotmani</div></b>
-
+            <div class="image-caption">
+              <b>
+                <div class="title-caption">Fakta Terkini Kasus Vina Cirebon, Polemik Pegi hingga Langkah Hotmani</div>
+              </b>
+              <div class="news-container mt-3">
+                  <h5 class="sub-caption">Berita.news</h5>
+                  <h6 class="time-news">4 jam yang lalu</h6>
+                  <button class="btn btn-primary">Selengkapnya</button>
+              </div>
+            </div>
+          </div>
+          
+          <div class="news-feed mt-4">
+            <div class="news-feed-title d-flex justify-content-between">
+              <h5 class="title">News Feed</h5>
+              <button class="btn btn-primary">See more</button>
+            </div>
+            <div class="row mt-4">
+              <div class="col-sm-4 mb-3 mb-sm-0">
+                <div class="card terpopuler" style="width: 18rem;">
+                  <img src="https://awsimages.detik.net.id/community/media/visual/2024/03/29/vina-sebelum-7-hari_169.jpeg?w=1200" class="card-img-top" alt="...">
+                  <div class="title-terpopuler mt-1">
+                    <h5>Jakarta Masih Jadi Kota Terbaik Se-Indonesia</h5>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-4">
+                <div class="card terpopuler" style="width: 18rem;">
+                  <img src="https://awsimages.detik.net.id/community/media/visual/2024/03/29/vina-sebelum-7-hari_169.jpeg?w=1200" class="card-img-top" alt="...">
+                  <div class="title-terpopuler mt-1">
+                    <h5>Jakarta Masih Jadi Kota Terbaik Se-Indonesia</h5>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-4">
+                <div class="card terpopuler" style="width: 18rem;">
+                  <img src="https://awsimages.detik.net.id/community/media/visual/2024/03/29/vina-sebelum-7-hari_169.jpeg?w=1200" class="card-img-top" alt="...">
+                  <div class="title-terpopuler mt-1">
+                    <h5>Jakarta Masih Jadi Kota Terbaik Se-Indonesia</h5>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-4">
+                <div class="card terpopuler" style="width: 18rem;">
+                  <img src="https://awsimages.detik.net.id/community/media/visual/2024/03/29/vina-sebelum-7-hari_169.jpeg?w=1200" class="card-img-top" alt="...">
+                  <div class="title-terpopuler mt-1">
+                    <h5>Jakarta Masih Jadi Kota Terbaik Se-Indonesia</h5>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-4">
+                <div class="card terpopuler" style="width: 18rem;">
+                  <img src="https://awsimages.detik.net.id/community/media/visual/2024/03/29/vina-sebelum-7-hari_169.jpeg?w=1200" class="card-img-top" alt="...">
+                  <div class="title-terpopuler mt-1">
+                    <h5>Jakarta Masih Jadi Kota Terbaik Se-Indonesia</h5>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-4">
+                <div class="card terpopuler" style="width: 18rem;">
+                  <img src="https://awsimages.detik.net.id/community/media/visual/2024/03/29/vina-sebelum-7-hari_169.jpeg?w=1200" class="card-img-top" alt="...">
+                  <div class="title-terpopuler mt-1">
+                    <h5>Jakarta Masih Jadi Kota Terbaik Se-Indonesia</h5>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+  
         <div class="col-2">
-          <h5 class="title">Headline News</h5>
+            <h5 class="title">Terpopuler</h5>
+              <div class="card terpopuler" style="width: 16rem;">
+                <img src="https://awsimages.detik.net.id/community/media/visual/2024/03/29/vina-sebelum-7-hari_169.jpeg?w=1200" class="card-img-top" alt="...">
+                <div class="title-terpopuler mt-1">
+                  <h5>Jakarta Masih Jadi Kota Terbaik Se-Indonesia</h5>
+                </div>
+              </div>
+              <div class="card terpopuler mt-3" style="width: 16rem;">
+                <img src="https://awsimages.detik.net.id/community/media/visual/2024/03/29/vina-sebelum-7-hari_169.jpeg?w=1200" class="card-img-top" alt="...">
+                <div class="title-terpopuler mt-1">
+                  <h5>Jakarta Masih Jadi Kota Terbaik Se-Indonesia</h5>
+                </div>
+              </div>
+              <div class="card terpopuler mt-3" style="width: 16rem;">
+                <img src="https://awsimages.detik.net.id/community/media/visual/2024/03/29/vina-sebelum-7-hari_169.jpeg?w=1200" class="card-img-top" alt="...">
+                <div class="title-terpopuler mt-1">
+                  <h5>Jakarta Masih Jadi Kota Terbaik Se-Indonesia</h5>
+                </div>
+              </div>
+              <div class="card terpopuler mt-3" style="width: 16rem;">
+                <img src="https://awsimages.detik.net.id/community/media/visual/2024/03/29/vina-sebelum-7-hari_169.jpeg?w=1200" class="card-img-top" alt="...">
+                <div class="title-terpopuler mt-1">
+                  <h5>Jakarta Masih Jadi Kota Terbaik Se-Indonesia</h5>
+                </div>
+              </div>
+              <div class="card terpopuler mt-3" style="width: 16rem;">
+                <img src="https://awsimages.detik.net.id/community/media/visual/2024/03/29/vina-sebelum-7-hari_169.jpeg?w=1200" class="card-img-top" alt="...">
+                <div class="title-terpopuler mt-1">
+                  <h5>Jakarta Masih Jadi Kota Terbaik Se-Indonesia</h5>
+                </div>
+              </div>
+        </div>
+      </div>
+  
+      <div class="category-news mt-5">
+        <div class="category-content">
+          <div class="title-content d-flex justify-content-between">
+            <h5 class="title">Sport News</h5>
+            <i class="bi bi-arrow-right-circle-fill"></i>
+          </div>
+          <hr>
+          <div class="news d-flex">
+            <img src="https://awsimages.detik.net.id/community/media/visual/2024/03/29/vina-sebelum-7-hari_169.jpeg?w=1200" alt="">
+            <div class="news-title">
+              <h2>Fakta Terkini Kasus Vina Cirebon, Polemik Pegi hingga Langkah Hotman</h2>
+              <h6 class="text-secondary mt-4">10 menit yang lalu</h6>
+            </div>
+          </div>
+        </div>
+        <div class="category-content mt-5">
+          <div class="title-content d-flex justify-content-between">
+            <h5 class="title">Sport News</h5>
+            <i class="bi bi-arrow-right-circle-fill"></i>
+          </div>
+          <hr>
+          <div class="news d-flex">
+            <img src="https://awsimages.detik.net.id/community/media/visual/2024/03/29/vina-sebelum-7-hari_169.jpeg?w=1200" alt="">
+            <div class="news-title">
+              <h2>Fakta Terkini Kasus Vina Cirebon, Polemik Pegi hingga Langkah Hotman</h2>
+              <h6 class="text-secondary mt-4">10 menit yang lalu</h6>
+            </div>
+          </div>
+        </div>
+        <div class="category-content mt-5">
+          <div class="title-content d-flex justify-content-between">
+            <h5 class="title">Sport News</h5>
+            <i class="bi bi-arrow-right-circle-fill"></i>
+          </div>
+          <hr>
+          <div class="news d-flex">
+            <img src="https://awsimages.detik.net.id/community/media/visual/2024/03/29/vina-sebelum-7-hari_169.jpeg?w=1200" alt="">
+            <div class="news-title">
+              <h2>Fakta Terkini Kasus Vina Cirebon, Polemik Pegi hingga Langkah Hotman</h2>
+              <h6 class="text-secondary mt-4">10 menit yang lalu</h6>
+            </div>
+          </div>
+        </div>
+        <div class="category-content mt-5">
+          <div class="title-content d-flex justify-content-between">
+            <h5 class="title">Sport News</h5>
+            <i class="bi bi-arrow-right-circle-fill"></i>
+          </div>
+          <hr>
+          <div class="news d-flex">
+            <img src="https://awsimages.detik.net.id/community/media/visual/2024/03/29/vina-sebelum-7-hari_169.jpeg?w=1200" alt="">
+            <div class="news-title">
+              <h2>Fakta Terkini Kasus Vina Cirebon, Polemik Pegi hingga Langkah Hotman</h2>
+              <h6 class="text-secondary mt-4">10 menit yang lalu</h6>
+            </div>
+          </div>
+        </div>
+        <div class="category-content mt-5">
+          <div class="title-content d-flex justify-content-between">
+            <h5 class="title">Sport News</h5>
+            <i class="bi bi-arrow-right-circle-fill"></i>
+          </div>
+          <hr>
+          <div class="news d-flex">
+            <img src="https://awsimages.detik.net.id/community/media/visual/2024/03/29/vina-sebelum-7-hari_169.jpeg?w=1200" alt="">
+            <div class="news-title">
+              <h2>Fakta Terkini Kasus Vina Cirebon, Polemik Pegi hingga Langkah Hotman</h2>
+              <h6 class="text-secondary mt-4">10 menit yang lalu</h6>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </template>
   
   <style>
-  .margin-top {
+  .margin-top{
     margin-top: 100px;
   }
-
-  .title{
+    .title{
+        color: #AB533C;
+        font-size: 25px;
+    }
+    .image-news {
+      position: relative;
+    }
+  
+    .image-news img {
+      width: 100%;
+      height: 576px;
+      background-color: #f0f0f0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border: 1px solid #ddd;
+      border-radius: 15px;
+    }
+  
+    .image-caption {
+      position: absolute;
+      bottom: 40px;
+      padding: 0 40px 0;
+      color: #ffffff;
+    }
+  
+    .title-caption{
+      font-size: 36px;
+    }
+  
+    .sub-caption{
       color: #AB533C;
-  }
-  .image-news {
-    position: relative;
-  }
-
-  .image-news img {
-    width: 100%;
-    height: 576px;
-    background-color: #f0f0f0;
+      margin-right: 20px;
+    }
+  
+    .time-news{
+      margin-right: 20px;
+    }
+    
+    .news-container {
     display: flex;
-    justify-content: center;
     align-items: center;
-    border: 1px solid #ddd;
-    border-radius: 15px;
-  }
-
-  .image-caption {
-    position: absolute;
-    font-size: 36px;
-    bottom: 150px;
-    padding: 0 40px 0;
-    color: #ffffff;
-  }
-
-  .image-news::after {
-    content: "";
-    position: absolute;
-    top: 0;
-    right: -15px;
-    height: 100%;
-    width: 1px;
-    background-color: #000000; 
-    opacity: 50%;
-  }
+    }
+  
+    .image-news::after {
+      content: "";
+      position: absolute;
+      top: 0;
+      right: -15px;
+      height: 100%;
+      width: 1px;
+      background-color: #000000; 
+      opacity: 50%;
+    }
+  
+    .card.terpopuler{
+      border: none;
+    }
+  
+    .title-terpopuler h5{
+      font-size: 14px;
+    }
+  
+    .news-feed-title{
+      align-items: center;
+    }
+    
+    .category-content hr{
+      width: 25%;
+    }
+  
+    .title-content{
+      width: 25%;
+      align-items: center;
+    }
+  
+    .title-content i{
+      font-size: 25px;
+    }
+  
+    .news{
+      align-items: center;
+    }
+  
+    .news img{
+      width: 50%;
+      margin-right: 50px;
+    }
   </style>
+  
+  
