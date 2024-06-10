0af3b156f78be7b9d2e7e41d9606c973063b983b
afifahnaurak <afifahnaurak@gmail.com>
2024-06-10 11:53:33 +0700

    feat(news): update ui news page

--- frontend/src/components/News.vue
+++ frontend/src/components/News.vue
@@ -15,30 +15,93 @@
     <h1 class="title-news">Fakta Terkini Kasus Vina Cirebon, Polemik Pegi hingga Langkah Hotmani</h1>
     <div class="content">
       <p>
-        <strong>Jakarta, Berita.com</strong> - Paragraf 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <strong>Jakarta, Berita.com</strong> - Paragraf 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+        do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
       <div class="image-container">
-        <img src="https://awsimages.detik.net.id/community/media/visual/2024/03/29/vina-sebelum-7-hari_169.jpeg?w=1200" alt="Image 1">
-        <img src="https://awsimages.detik.net.id/community/media/visual/2024/03/29/vina-sebelum-7-hari_169.jpeg?w=1200" alt="Image 2">
+        <img src="https://awsimages.detik.net.id/community/media/visual/2024/03/29/vina-sebelum-7-hari_169.jpeg?w=1200"
+          alt="Image 1">
+        <img src="https://awsimages.detik.net.id/community/media/visual/2024/03/29/vina-sebelum-7-hari_169.jpeg?w=1200"
+          alt="Image 2">
       </div>
       <p>
-       Paragraf 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Paragraf 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum.
       </p>
     </div>
     <div class="button-container">
-        <button class="circle-button">
-          <i class="bi bi-share-fill button-icon"></i>
-        </button>
-        <button class="circle-button">
-          <i class="bi bi-bookmark-fill button-icon"></i>
-        </button>
+      <button class="circle-button">
+        <i class="bi bi-share-fill button-icon"></i>
+      </button>
+      <button class="circle-button">
+        <i class="bi bi-bookmark-fill button-icon"></i>
+      </button>
+    </div>
+    <h4 class="heading">Comments</h4>
+    <div class="container-comment">
+      <div class="add-comment">
+        <p class="title-comment">Tulis Komentar Anda</p>
+        <textarea class="comment-textarea" placeholder="Add a comment..."></textarea>
+        <button class="send-button" @click="sendComment">Kirim</button>
       </div>
     </div>
+    <div class="comments">
+      <div>
+        <img src="https://via.placeholder.com/125" size="10rem" class="profile-picture" />
+      </div>
+      <div class="content-comment">
+        <div class="title-time">
+          <p class="username-comment">Nama User</p>
+          <p class="time-comment">48 Minutes Ago</p>
+        </div>
+        <p class="user-comment">Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You’ve nailed the design and the responsiveness at various breakpoints works really well.</p>
+      </div>
+    </div>
+    <div class="comments">
+      <div>
+        <img src="https://via.placeholder.com/125" size="10rem" class="profile-picture" />
+      </div>
+      <div class="content-comment">
+        <div class="title-time">
+          <p class="username-comment">Nama User</p>
+          <p class="time-comment">48 Minutes Ago</p>
+        </div>
+        <p class="user-comment">Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You’ve nailed the design and the responsiveness at various breakpoints works really well.</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'News',
+  methods: {
+    sendComment() {
+      alert('Comment sent!');
+    }
+  }
 }
 </script>
 
@@ -133,6 +196,99 @@ export default {
 
 .circle-button:hover {
   background-color: #023362;
+}
+
+.heading {
+  font-weight: bold;
+}
+
+.container-comment {
+  background-color: #D9D9D9;
+  width: full;
+  height: 300px;
+  border-radius: 10px;
+  margin-top: 25px;
+}
+
+.add-comment {
+  padding: 25px;
+}
+
+.title-comment {
+  font-weight: 700;
+  font-size: 18px;
+}
+
+.comment-textarea {
+  width: 100%;
+  height: 150px;
+  padding: 10px;
+  border-radius: 8px;
+  border: 1px solid #ccc;
+  resize: none;
+  margin-bottom: 10px;
+}
+
+.send-button {
+  background-color: #085487;
+  color: white;
+  border: 0;
+  border-radius: 8px;
+  padding: 10px 20px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+  float: right;
+}
+
+.send-button:hover {
+  background-color: #023362;
+}
+
+.comments {
+  width: full;
+  height: 110px;
+  border-radius: 10px;
+  border: 20px;
+  border: 1px solid #ccc;
+  margin-top: 25px;
+  padding: 0 25px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.content-comment {
+  padding-left: 20px;
+  padding-top: 15px;
+}
+
+.profile-picture {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  object-fit: cover;
+}
+
+.title-time {
+  display: flex;
+  justify-content: space-between;
+}
+
+.username-comment {
+  font-weight: 700;
+  font-size: 16px;
+  margin: 0;
+}
+
+.time-comment {
+  font-size: 12px;
+  margin: 6px 0;
+}
+
+.user-comment {
+  font-size: 14px;
+  color: #67727E;
 }
 
 @media (max-width: 426px) {
