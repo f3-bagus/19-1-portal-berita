daa6ddf9bd94448c9a3874ead399bcaf779ef48e
nabillastywn <116862851+nabillastywn@users.noreply.github.com>
2024-06-16 23:39:49 +0700

    refactor(router): refactor user router so the name accessible in news

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,19 +1,18 @@
 import express from "express";
 import {
-    getUsers,
-    getUserById,
-    createUser,
-    updateUser,
-    deleteUsers
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUsers,
 } from "../controllers/Users.js";
 import { verifyToken, isAdmin } from "../middleware/Auth.js";
 
-
 const router = express.Router();
 
-router.get('/users', verifyToken, isAdmin, getUsers);
-router.get('/users/:id', verifyToken, getUserById);
-router.post('/users', verifyToken, isAdmin, createUser);
-router.patch('/users/:id', verifyToken, updateUser);
-router.delete('/users/:id', verifyToken, isAdmin, deleteUsers);
+router.get("/users", verifyToken, isAdmin, getUsers);
+router.get("/users/:id", getUserById);
+router.post("/users", verifyToken, isAdmin, createUser);
+router.patch("/users/:id", verifyToken, updateUser);
+router.delete("/users/:id", verifyToken, isAdmin, deleteUsers);
 export default router;
