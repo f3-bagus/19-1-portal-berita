1a64755f0f9d31573cc1cd57c1af672d54201259
afifahnaurak <afifahnaurak@gmail.com>
2024-06-16 13:52:50 +0700

    fix(auth): token authorization

--- frontend/services/axios.js
+++ frontend/services/axios.js
@@ -1,8 +1,7 @@
-
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/',
+  baseURL:  'http://localhost:5000/',
 });
 
 const handleRemoveToken = async () => {
@@ -14,28 +13,26 @@ const handleRemoveToken = async () => {
 };
 
 instance.interceptors.request.use(
-    config => {
-      const token = localStorage.getItem('token');
-      if (token) {
-        console.log(token);
-        config.headers.Authorization = `Bearer ${token}`;
-        console.log('Authorization Header:', config.headers['Authorization']);
-      }
-      return config;
-    },
-    error => {
-      return Promise.reject(error);
+  async (config) => {
+    const token = await localStorage.getItem('token');
+    if (token && config.headers) {
+      config.headers.Authorization = "Bearer " + token;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  );
+);
 
-// instance.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response) {
-//       handleRemoveToken();
-//     }
-//     return Promise.reject(error);
-//   }
-// );
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      handleRemoveToken();
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
