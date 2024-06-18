15507ce161facc900a2367425fc237cceaf3ea68
afifahnaurak <afifahnaurak@gmail.com>
2024-06-18 19:26:40 +0700

    feat(admin): add api configuration on manage messages

--- frontend/src/views/admin/ManageMessages.vue
+++ frontend/src/views/admin/ManageMessages.vue
@@ -5,13 +5,6 @@
         <div class="container-fluid px-4">
           <h1 class="mt-4 mb-4">Manage Messages</h1>
           <div class="container-message mb-4">
-            <button
-              variant="primary"
-              class="btn btn-success"
-              @click="showCreateModal = true"
-            >
-              Create Message
-            </button>
             <div class="table-responsive">
               <table class="table table-light mt-3 table-rounded">
                 <thead>
@@ -24,29 +17,17 @@
                   </tr>
                 </thead>
                 <tbody>
-                  <tr v-for="(message, index) in messages" :key="index">
+                  <tr v-for="(message, index) in messages" :key="message.message_id">
                     <th scope="row">{{ index + 1 }}</th>
-                    <td>{{ message.name }}</td>
-                    <td>{{ message.email }}</td>
-                    <td>{{ message.message }}</td>
+                    <td>{{ message.user.username }}</td>
+                    <td>{{ message.user.email }}</td>
+                    <td>{{ message.message_text }}</td>
                     <td class="gap-2">
                       <div class="d-flex flex-column flex-sm-row gap-2">
-                        <button
-                          class="btn btn-warning"
-                          @click="showUpdateModal(message)"
-                        >
-                          Update
-                        </button>
-                        <button
-                          class="btn btn-danger"
-                          @click="deleteMessage(index)"
-                        >
+                        <button class="btn btn-danger" @click="deleteMessage(message.message_id, index)">
                           Delete
                         </button>
-                        <button
-                          class="btn btn-info"
-                          @click="showReplyModal(message)"
-                        >
+                        <button class="btn btn-info" @click="showReplyModal(message)">
                           Reply
                         </button>
                       </div>
@@ -58,117 +39,13 @@
           </div>
         </div>
 
-        <!-- Create Message Modal -->
-        <div class="modal" tabindex="-1" v-if="showCreateModal">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title">Create Message</h5>
-                <button class="btn btn-danger" @click="showCreateModal = false">
-                  <i class="bi bi-x-lg"></i>
-                </button>
-              </div>
-              <div class="modal-body">
-                <form @submit.prevent="createMessage">
-                  <div class="mb-3">
-                    <label for="name" class="form-label">Name</label>
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="name"
-                      v-model="newMessage.name"
-                      required
-                    />
-                  </div>
-                  <div class="mb-3">
-                    <label for="email" class="form-label">Email</label>
-                    <input
-                      type="email"
-                      class="form-control"
-                      id="email"
-                      v-model="newMessage.email"
-                      required
-                    />
-                  </div>
-                  <div class="mb-3">
-                    <label for="message" class="form-label">Message</label>
-                    <textarea
-                      class="form-control"
-                      id="message"
-                      v-model="newMessage.message"
-                      required
-                    ></textarea>
-                  </div>
-                  <button type="submit" class="btn btn-success">Create</button>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <!-- Update Message Modal -->
-        <div class="modal" tabindex="-1" v-if="showUpdateModalFlag">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title">Update Message</h5>
-                <button
-                  class="btn btn-danger"
-                  @click="showUpdateModalFlag = false"
-                >
-                  <i class="bi bi-x-lg"></i>
-                </button>
-              </div>
-              <div class="modal-body">
-                <form @submit.prevent="updateMessage">
-                  <div class="mb-3">
-                    <label for="update-name" class="form-label">Name</label>
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="update-name"
-                      v-model="currentMessage.name"
-                      required
-                    />
-                  </div>
-                  <div class="mb-3">
-                    <label for="update-email" class="form-label">Email</label>
-                    <input
-                      type="email"
-                      class="form-control"
-                      id="update-email"
-                      v-model="currentMessage.email"
-                      required
-                    />
-                  </div>
-                  <div class="mb-3">
-                    <label for="update-message" class="form-label"
-                      >Message</label
-                    >
-                    <textarea
-                      class="form-control"
-                      id="update-message"
-                      v-model="currentMessage.message"
-                      required
-                    ></textarea>
-                  </div>
-                  <button type="submit" class="btn btn-warning">Update</button>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-
         <!-- Reply Message Modal -->
         <div class="modal" tabindex="-1" v-if="showReplyModalFlag">
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
                 <h5 class="modal-title">Reply to Message</h5>
-                <button
-                  class="btn btn-danger"
-                  @click="showReplyModalFlag = false"
-                >
+                <button class="btn btn-danger" @click="showReplyModalFlag = false">
                   <i class="bi bi-x-lg"></i>
                 </button>
               </div>
@@ -176,45 +53,19 @@
                 <form @submit.prevent="replyMessage">
                   <div class="mb-3">
                     <label for="reply-name" class="form-label">Name</label>
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="reply-name"
-                      v-model="replyMessageDetails.name"
-                      disabled
-                    />
+                    <input type="text" class="form-control" id="reply-name" v-model="replyMessageDetails.user.username" disabled />
                   </div>
                   <div class="mb-3">
                     <label for="reply-email" class="form-label">Email</label>
-                    <input
-                      type="email"
-                      class="form-control"
-                      id="reply-email"
-                      v-model="replyMessageDetails.email"
-                      disabled
-                    />
+                    <input type="email" class="form-control" id="reply-email" v-model="replyMessageDetails.user.email" disabled />
                   </div>
                   <div class="mb-3">
-                    <label for="reply-original-message" class="form-label"
-                      >Original Message</label
-                    >
-                    <textarea
-                      class="form-control"
-                      id="reply-original-message"
-                      v-model="replyMessageDetails.message"
-                      disabled
-                    ></textarea>
+                    <label for="reply-original-message" class="form-label">Original Message</label>
+                    <textarea class="form-control" id="reply-original-message" v-model="replyMessageDetails.message_text" disabled></textarea>
                   </div>
                   <div class="mb-3">
-                    <label for="reply-message" class="form-label"
-                      >Your Reply</label
-                    >
-                    <textarea
-                      class="form-control"
-                      id="reply-message"
-                      v-model="replyMessageDetails.reply"
-                      required
-                    ></textarea>
+                    <label for="reply-message" class="form-label">Your Reply</label>
+                    <textarea class="form-control" id="reply-message" v-model="replyMessageDetails.reply" required></textarea>
                   </div>
                   <button type="submit" class="btn btn-info">Send Reply</button>
                 </form>
@@ -228,98 +79,61 @@
 </template>
 
 <script>
-import AdminLayout from "../../components/Admin/AdminLayout.vue";
+import axios from '../../../services/axios';
+import AdminLayout from '../../components/Admin/AdminLayout.vue';
 
 export default {
-  name: "ManageMessages",
+  name: 'ManageMessages',
   components: {
     AdminLayout,
   },
-  created() {
-    const userRole = localStorage.getItem("userRole");
-    if (userRole !== "admin") {
-      this.$router.push({
-        name: "LandingPage",
-      });
-    }
-  },
   data() {
     return {
-      showCreateModal: false,
-      showUpdateModalFlag: false,
+      messages: [],
       showReplyModalFlag: false,
-      newMessage: {
-        name: "",
-        email: "",
-        message: "",
-      },
-      currentMessage: null,
-      replyMessageDetails: {
-        name: "",
-        email: "",
-        message: "",
-        reply: "",
-      },
-      messages: [
-        {
-          name: "User 1",
-          email: "user1@example.com",
-          message: "Hello, this is a message.",
-        },
-        {
-          name: "User 2",
-          email: "user2@example.com",
-          message: "This is another message.",
-        },
-        // Add more messages as needed
-      ],
+      replyMessageDetails: {},
     };
   },
-  methods: {
-    createMessage() {
-      this.messages.push({
-        ...this.newMessage,
+  created() {
+    const userRole = localStorage.getItem('userRole');
+    if (userRole !== 'admin') {
+      this.$router.push({
+        name: 'LandingPage',
       });
-      this.showCreateModal = false;
-      this.newMessage = {
-        name: "",
-        email: "",
-        message: "",
-      };
-    },
-    showUpdateModal(message) {
-      this.currentMessage = {
-        ...message,
-      };
-      this.showUpdateModalFlag = true;
-    },
-    updateMessage() {
-      const index = this.messages.findIndex(
-        (msg) => msg.email === this.currentMessage.email
-      );
-      if (index !== -1) {
-        this.messages.splice(index, 1, {
-          ...this.currentMessage,
-        });
+    } else {
+      this.fetchMessages();
+    }
+  },
+  methods: {
+    async fetchMessages() {
+      try {
+        const response = await axios.get('/admin/contact');
+        this.messages = response.data;
+      } catch (error) {
+        console.error('Error fetching messages:', error);
       }
-      this.showUpdateModalFlag = false;
     },
-    deleteMessage(index) {
-      this.messages.splice(index, 1);
+    async deleteMessage(message_id, index) {
+      try {
+        await axios.delete(`/admin/contact/${message_id}`);
+        this.messages.splice(index, 1); // Remove message from array
+        alert('Message deleted successfully');
+      } catch (error) {
+        console.error('Error deleting message:', error);
+      }
     },
     showReplyModal(message) {
-      this.replyMessageDetails = {
-        ...message,
-        reply: "",
-      };
       this.showReplyModalFlag = true;
+      this.replyMessageDetails = { ...message };
     },
-    replyMessage() {
-      // Handle the reply (e.g., send an email or store the reply)
-      console.log(
-        `Reply sent to ${this.replyMessageDetails.email}: ${this.replyMessageDetails.reply}`
-      );
-      this.showReplyModalFlag = false;
+    async replyMessage() {
+      try {
+        // Implement your logic to send reply using POST Axios request
+        // After successful reply, close the modal
+        this.showReplyModalFlag = false;
+      } catch (error) {
+        console.error('Error replying message:', error);
+      }
     },
   },
 };
