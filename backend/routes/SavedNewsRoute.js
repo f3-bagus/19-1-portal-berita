366350ebb6a4f781db598fcf857b9d05f7544110
afifahnaurak <afifahnaurak@gmail.com>
2024-06-19 19:45:32 +0700

    feat(saved-news): update route and controller for delete saved news

--- backend/routes/SavedNewsRoute.js
+++ backend/routes/SavedNewsRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { saveNews, getSavedNews, getSavedNewsAdmin } from '../controllers/SavedNews.js';
+import { saveNews, getSavedNews, getSavedNewsAdmin, deleteSavedNews } from '../controllers/SavedNews.js';
 import { verifyToken, isUser, isAdmin } from "../middleware/Auth.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post('/saved-news', verifyToken, isUser, saveNews);
 router.get('/saved-news', verifyToken, isUser, getSavedNews);
 router.get('/saved/admin/:user_id', verifyToken, isAdmin, getSavedNewsAdmin);
+router.delete('/saved-news/:id', verifyToken, deleteSavedNews);
 
 export default router;
