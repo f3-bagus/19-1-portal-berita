5cac318a3defe0d77359ff5bc93f47aad846a7fc
nabillastywn <116862851+nabillastywn@users.noreply.github.com>
2024-06-19 15:34:53 +0700

    feat(comments): API integration for comments

--- frontend/src/components/News.vue
+++ frontend/src/components/News.vue
@@ -1,87 +1,77 @@
 <template>
   <div class="container margin-top">
-    <div class="breadcrumbs">
-      <span>News > Nasional</span>
+    <div class="content-wrapper">
+      <h1>{{ title }}</h1>
+      <div class="metadata">
+        <p class="author">Author: {{ author }}</p>
+        <p class="date">{{ date }}</p>
+      </div>
+      <div v-if="imageUrl" class="image-news">
+        <img :src="imageUrl" alt="News Image" />
+      </div>
+      <div class="content">
+        <p>{{ content }}</p>
+      </div>
     </div>
-    <div class="author">{{ author }}</div>
-    <div class="metadata">
-      <div class="date">{{ date }}</div>
-    </div>
-    <img :src="imageUrl" class="image-news" alt="News Image" />
-    <h1 class="title-news">{{ title }}</h1>
-    <div class="content">
-      <p>{{ content }}</p>
-    </div>
-    <div class="button-container">
-      <button class="circle-button" @click="showShareModal">
-        <i class="bi bi-share-fill button-icon"></i>
-      </button>
-      <button class="circle-button">
-        <i class="bi bi-bookmark-fill button-icon"></i>
-      </button>
-    </div>
-    <h4 class="heading">Comments</h4>
+
     <div class="container-comment">
       <div class="add-comment">
         <p class="title-comment">Tulis Komentar Anda</p>
-        <textarea class="comment-textarea" placeholder="Add a comment..."></textarea>
+        <textarea
+          v-model="newComment"
+          class="comment-textarea"
+          placeholder="Add a comment..."
+        ></textarea>
         <button class="send-button" @click="sendComment">Kirim</button>
       </div>
     </div>
-    <div class="comments">
+
+    <div v-for="comment in comments" :key="comment.comment_id" class="comments">
       <div>
-        <img src="https://via.placeholder.com/125" size="10rem" class="profile-picture" />
+        <img
+          src="https://via.placeholder.com/125"
+          class="profile-picture"
+          alt="Profile Picture"
+        />
       </div>
       <div class="content-comment">
         <div class="title-time">
-          <p class="username-comment">Nama User</p>
-          <p class="time-comment">48 Minutes Ago</p>
+          <p class="username-comment">{{ comment.user.username }}</p>
+          <p class="time-comment">{{ formatDate(comment.createdAt) }}</p>
         </div>
-        <p class="user-comment">Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You’ve nailed the design and the responsiveness at various breakpoints works really well.</p>
-        <p class="text-right text-success">Reply</p>
-      </div>
-    </div>
-    <!-- Share Modal -->
-    <div v-if="isShareModalVisible" class="share-modal">
-      <div class="share-modal-content">
-        <button class="btn btn-danger mb-3 close-button" @click="hideShareModal">
-          <i class="bi bi-x-lg"></i>
-        </button>
-        <div class="share-options">
-          <button class="share-option" @click="shareToFacebook"><i class="bi bi-facebook"></i> Facebook</button>
-          <button class="share-option" @click="shareToTwitter"><i class="bi bi-twitter"></i> Twitter</button>
-          <button class="share-option" @click="shareToWhatsApp"><i class="bi bi-whatsapp"></i> WhatsApp</button>
-          <button class="share-option" @click="shareToLine"><i class="bi bi-line"></i> Line</button>
-          <button class="share-option" @click="shareToTelegram"><i class="bi bi-telegram"></i> Telegram</button>
-          <button class="share-option" @click="copyLink"><i class="bi bi-link-45deg"></i> Copy Link</button>
-        </div>
+        <p class="user-comment">{{ comment.comment_text }}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "../../services/axios";
+import moment from "moment";
 
 export default {
   name: "News",
   data() {
     return {
-      title: '',
-      content: '',
-      author: '',
-      imageUrl: '',
-      date: '',
-      isShareModalVisible: false,
+      title: "",
+      content: "",
+      author: "",
+      imageUrl: "",
+      date: "",
+      newComment: "",
+      comments: [],
     };
   },
   created() {
     this.fetchNews();
+    this.fetchComments();
   },
   methods: {
     async fetchNews() {
       try {
-        const response = await axios.get(`http://localhost:5000/news/${this.$route.params.id}`);
+        const response = await axios.get(
+          `http://localhost:5000/news/${this.$route.params.id}`
+        );
         const data = response.data;
         this.title = data.title;
         this.content = data.content;
@@ -89,42 +79,46 @@ export default {
         this.date = this.formatDate(data.createdAt);
         this.author = data.author.username;
       } catch (error) {
-        console.error('Error fetching news:', error);
+        console.error("Error fetching news:", error);
+      }
+    },
+    async fetchComments() {
+      try {
+        const response = await axios.get(
+          `http://localhost:5000/comments/${this.$route.params.id}`
+        );
+        this.comments = response.data;
+      } catch (error) {
+        console.error("Error fetching comments:", error);
+      }
+    },
+    async sendComment() {
+      try {
+        const accessToken = localStorage.getItem("accessToken");
+        if (!accessToken) {
+          throw new Error("Access token not found");
+        }
+
+        const response = await axios.post(
+          "http://localhost:5000/comments",
+          {
+            news_id: this.$route.params.id,
+            comment_text: this.newComment,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+        this.newComment = "";
+        this.fetchComments();
+      } catch (error) {
+        console.error("Error sending comment:", error);
       }
     },
     formatDate(dateString) {
-      return moment(dateString).format('dddd, D MMMM YYYY HH.mm [WIB]');
-    },
-    sendComment() {
-      alert("Comment sent!");
-    },
-    showShareModal() {
-      this.isShareModalVisible = true;
-    },
-    hideShareModal() {
-      this.isShareModalVisible = false;
-    },
-    shareToFacebook() {
-      window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(window.location.href));
-    },
-    shareToTwitter() {
-      window.open('https://twitter.com/intent/tweet?url=' + encodeURIComponent(window.location.href));
-    },
-    shareToWhatsApp() {
-      window.open('https://api.whatsapp.com/send?text=' + encodeURIComponent(window.location.href));
-    },
-    shareToLine() {
-      window.open('https://line.me/R/msg/text/?' + encodeURIComponent(window.location.href));
-    },
-    shareToTelegram() {
-      window.open('https://telegram.me/share/url?url=' + encodeURIComponent(window.location.href));
-    },
-    copyLink() {
-      navigator.clipboard.writeText(window.location.href).then(() => {
-        alert('Link copied to clipboard!');
-      }).catch((error) => {
-        console.error('Failed to copy: ', error);
-      });
+      return moment(dateString).format("dddd, D MMMM YYYY HH.mm [WIB]");
     },
   },
 };
@@ -145,11 +139,11 @@ export default {
   border-radius: 15px;
 }
 
-.breadcrumbs {
-  margin-top: 10px;
-  color: #333;
-  font-weight: 700;
+.metadata {
   font-size: 16px;
+  display: flex;
+  justify-content: flex-start;
+  gap: 15px;
 }
 
 .author {
@@ -159,21 +153,8 @@ export default {
   font-weight: bold;
 }
 
-.metadata {
-  font-size: 16px;
-  display: flex;
-  justify-content: flex-start;
-  gap: 15px;
-}
-
-.date,
-.time {
+.date {
   color: #a6a6a6;
-}
-
-.title-news {
-  margin-top: 15px;
-  font-weight: bold;
 }
 
 .content {
@@ -182,59 +163,28 @@ export default {
   line-height: 1.6;
 }
 
-.button-container {
-  display: flex;
-  justify-content: start;
-  gap: 10px;
-  margin-top: 30px;
-  margin-bottom: 50px;
-}
-
-.circle-button {
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-  border: none;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #085487;
-  transition: background-color 0.3s;
-}
-
-.button-icon {
-  font-size: 1.2rem;
-  color: white;
-}
-
-.circle-button:hover {
-  background-color: #023362;
-}
-
-.heading {
-  font-weight: bold;
-}
-
 .container-comment {
   background-color: #d9d9d9;
-  width: full;
-  height: 300px;
+  width: 100%;
+  height: auto;
   border-radius: 10px;
   margin-top: 25px;
+  padding: 20px;
 }
 
 .add-comment {
-  padding: 25px;
+  padding: 20px;
 }
 
 .title-comment {
   font-weight: 700;
   font-size: 18px;
+  margin-bottom: 10px;
 }
 
 .comment-textarea {
   width: 100%;
-  height: 150px;
+  height: 100px;
   padding: 10px;
   border-radius: 8px;
   border: 1px solid #ccc;
@@ -258,34 +208,32 @@ export default {
 }
 
 .comments {
-  width: full;
-  height: 140px;
+  width: 100%;
   border-radius: 10px;
-  border: 20px;
   border: 1px solid #ccc;
-  margin-top: 25px;
-  padding: 0 25px;
+  margin-top: 15px;
+  padding: 10px 15px;
   display: flex;
-  justify-content: space-between;
-}
-
-.content-comment {
-  padding-left: 20px;
-  padding-top: 15px;
+  justify-content: flex-start;
+  align-items: center;
 }
 
 .profile-picture {
-  margin-top: 1rem;
-  margin-bottom: 1rem;
   border-radius: 50%;
   width: 40px;
   height: 40px;
   object-fit: cover;
+  margin-right: 15px;
+}
+
+.content-comment {
+  flex-grow: 1;
 }
 
 .title-time {
   display: flex;
   justify-content: space-between;
+  margin-bottom: 5px;
 }
 
 .username-comment {
@@ -296,7 +244,7 @@ export default {
 
 .time-comment {
   font-size: 12px;
-  margin: 6px 0;
+  color: #a6a6a6;
 }
 
 .user-comment {
@@ -318,54 +266,5 @@ export default {
   .image-container img:last-child {
     margin-bottom: 0;
   }
-}
-
-/* Share Modal Styles */
-.share-modal {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: fixed;
-  z-index: 999;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-}
-
-.share-modal-content {
-  background-color: white;
-  padding: 20px;
-  border-radius: 10px;
-  width: 300px;
-  max-width: 80%;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-  position: relative; 
-}
-
-.close-button:hover {
-  color: black;
-}
-
-.share-options {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-}
-
-.share-option {
-  display: flex;
-  align-items: center;
-  gap: 10px;
-  padding: 10px;
-  background-color: #f0f0f0;
-  border-radius: 8px;
-  cursor: pointer;
-  transition: background-color 0.3s;
-}
-
-.share-option:hover {
-  background-color: #e0e0e0;
 }
 </style>
