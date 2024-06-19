7f7cdc786ce4a95d8df70518c063a37bfd988609
Add67 <addalhasyim@gmail.com>
2024-06-19 07:33:12 +0700

    rollback

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,18 @@
 import { Sequelize } from "sequelize";
 
+
 const db = new Sequelize("qFPVp1VCsAWYmj4H", "vV9e3ORVpZ2G3LLk", "272OQY5AHUhfcnwK", {
     host: "educalab.id",
     port: "3307",
     dialect: "mysql"
 });
+
+
+// const db = new Sequelize("qFPVp1VCsAWYmj4H", "vV9e3ORVpZ2G3LLk", "272OQY5AHUhfcnwK", {
+//     host: "educalab.id",
+//     port: "3307",
+//     dialect: "mysql"
+// });
 
 const connectDB = async () => {
     try {
