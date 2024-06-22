29f43b8b15f0cbff8d18967c049d95e552dbb073
Add67 <addalhasyim@gmail.com>
2024-06-22 19:18:38 +0700

    consume api gamelab

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://fe-msib-6-portal-berita-01.educalab.id/",
   })
 );
 app.use(bodyParser.json());
--- frontend/services/axios.js
+++ frontend/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:  'http://localhost:5000/',
+  baseURL:  'https://api-msib-6-portal-berita-01.educalab.id/',
 });
 
 const handleRemoveToken = async () => {
