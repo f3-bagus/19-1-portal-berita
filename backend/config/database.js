96a3b121ccabb3b5d3f70c6256d985cbb13178b7
Add67 <addalhasyim@gmail.com>
2024-06-22 20:13:07 +0700

    Gamelab Db connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,17 +1,17 @@
 import { Sequelize } from "sequelize";
 
 //Database CloudMysQl
-const db = new Sequelize("sql12713709", "sql12713709", "4q3rtyVSHv", {
-    host: "sql12.freemysqlhosting.net",
-    dialect: "mysql"
-});
-
-//Database Gamelab
-// const db = new Sequelize("qFPVp1VCsAWYmj4H", "vV9e3ORVpZ2G3LLk", "272OQY5AHUhfcnwK", {
-//     host: "educalab.id",
-//     port: "3307",
+// const db = new Sequelize("sql12713709", "sql12713709", "4q3rtyVSHv", {
+//     host: "sql12.freemysqlhosting.net",
 //     dialect: "mysql"
 // });
+
+//Database Gamelab
+const db = new Sequelize("qFPVp1VCsAWYmj4H", "vV9e3ORVpZ2G3LLk", "272OQY5AHUhfcnwK", {
+    host: "educalab.id",
+    port: "3307",
+    dialect: "mysql"
+});
 
 // const db = new Sequelize("berita_db", "root", "", {
 //         host: "localhost",
